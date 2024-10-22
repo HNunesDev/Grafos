38aab84f2ca5b25b5c1e098b33d0370241b81bf6
Henrique Nunes <henriqueguilhem77@gmail.com>
2024-10-22 20:07:46 -0300

    Adicao do grafo com laço e outro com todos os graficos estando interligados, também adicionei a criaçao da imagem e exibicao do mesmo

--- TeoriaGrafos.py
+++ TeoriaGrafos.py
@@ -9,12 +9,25 @@ grafo1 = Graph(edges = [(0,1), (1,2), (2,3), (3,0)], directed = True)
 grafo1.vs['label'] = range(grafo1.vcount())
 print(grafo1)
 
+
+#Todos os vertices estao ligados
 grafo2 = Graph(edges = [(0,1),(1,2),(2,3),(3,0),(3,2),(2,1),(1,0),(0,3)])
 grafo2.vs['label'] = range(grafo2.vcount())
 plot(grafo2, target="grafo2.png", bbox=(300, 300))
-print("Salvo em imgaem")
+print("Salvo em imagem do grafo 2")
 
 img = Image.open("grafo2.png")
 plt.imshow(img)
 plt.axis('off')  # Ocultar os eixos
+plt.show()
+
+#Grafo com laço
+grafo3 = Graph(edges = [(0,1), (1,2), (2,3), (3,0), (1,1)], directed = True)
+grafo3.vs['label'] = range(grafo3.vcount())
+plot(grafo3, target="grafo3.png", bbox=(300, 300))
+print("Salvo imagem 3")
+
+img2 = Image.open("grafo3.png")
+plt.imshow(img2)
+plt.axis('off')
 plt.show()
